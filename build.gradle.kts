1e2a301df894f3ac8f99f9e4647c618bdfe8c426
Ilya Vologin <ilyavologin@gmail.com>
2021-06-01 17:10:28 +0300

    Init commit

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.5.10"
+}
+
+group = "me.cupertank"
+version = "0.0.1"
+application {
+    mainClass.set("com.example.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-network:$ktor_version")
+}
---
+++ src/main/kotlin/me/cupertank/Application.kt
@@ -0,0 +1,138 @@
+package me.cupertank
+
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import io.ktor.utils.io.core.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+data class HttpRequest(
+    val requestBytes: ByteArray,
+    val method: String,
+    val host: String,
+    val port: Int,
+) {
+    companion object {
+        fun fromBytes(bytes: ByteArray): HttpRequest {
+            try {
+                val string = bytes.toString(Charsets.UTF_8)
+//                println(string)
+                val lines = string.lines()
+                val firstLineArgs = lines.first().split(" ")
+
+                val method = firstLineArgs[0]
+                val hostLine = lines.find { it.toLowerCase().startsWith("host:") } ?: ""
+                val hostLineArgs = hostLine.split(":")
+
+                return if (hostLineArgs.size == 3) {
+                    val (_, host, port) = hostLine.split(":")
+                    HttpRequest(bytes, method, host.trim(), port.toInt())
+                } else {
+                    val (_, host) = hostLine.split(":")
+                    HttpRequest(bytes, method, host.trim(), 80)
+                }
+            } catch (E: Exception) {
+                return EMPTY
+            }
+        }
+
+        val EMPTY = HttpRequest(ByteArray(0), "", "", -1)
+    }
+}
+
+
+fun main(args: Array<String>) {
+
+    runBlocking {
+        val selector = ActorSelectorManager(Dispatchers.Default)
+
+        val builder = aSocket(selector).tcp()
+
+        val server = builder.bind("0.0.0.0", 8080)
+        while (true) {
+            val clientSocket = server.accept()
+
+            launch {
+                try {
+                    val buffer = ByteArray(8192)
+                    println("Socket accepted: ${clientSocket.remoteAddress}")
+
+                    val clientReader = clientSocket.openReadChannel()
+                    val clientWriter = clientSocket.openWriteChannel(autoFlush = true)
+
+                    clientReader.awaitContent()
+                    var size = clientReader.readAvailable(buffer)
+
+                    val httpRequest = HttpRequest.fromBytes(buffer)
+                    if (buffer.isEmpty() || httpRequest == HttpRequest.EMPTY) {
+                        clientSocket.close()
+                        return@launch
+                    }
+
+                    println("HOST: ${httpRequest.host}:${httpRequest.port}")
+
+                    if (httpRequest.method == "CONNECT") {
+                        val serverSocket = try {
+                            builder.connect(httpRequest.host, httpRequest.port)
+                        } catch (e: Exception) {
+                            println("Failed CONNECT")
+                            clientSocket.close()
+                            return@launch
+                        }
+
+                        val serverReader = serverSocket.openReadChannel()
+                        val serverWriter = serverSocket.openWriteChannel(autoFlush = true)
+
+                        clientWriter.writeAvailable("HTTP/1.1 200 OK\r\n\r\n".toByteArray())
+                        clientWriter.flush()
+
+                        while (!clientReader.isClosedForRead && !clientWriter.isClosedForWrite
+                            && !serverReader.isClosedForRead && !serverReader.isClosedForWrite &&
+                            !clientSocket.isClosed && !serverSocket.isClosed
+                        ) {
+                            delay(50)
+                            while (clientReader.availableForRead > 0) {
+                                println("CLIENT = ${clientReader.availableForRead}")
+                                val size = clientReader.readAvailable(buffer)
+                                serverWriter.writeFully(buffer, 0, size)
+                                delay(25)
+                            }
+                            serverWriter.flush()
+
+                            delay(50)
+                            while (serverReader.availableForRead > 0) {
+                                println("SERVER = ${serverReader.availableForRead}")
+                                val size = serverReader.readAvailable(buffer)
+                                clientWriter.writeFully(buffer, 0, size)
+                                delay(25)
+                            }
+                            clientWriter.flush()
+                        }
+                        serverSocket.close()
+                        clientSocket.close()
+                    } else {
+                        val serverSocket = builder.connect(httpRequest.host, httpRequest.port)
+                        val serverWriter = serverSocket.openWriteChannel(autoFlush = true)
+                        val serverReader = serverSocket.openReadChannel()
+
+                        serverWriter.writeAvailable(buffer, 0, size)
+                        serverWriter.flush()
+
+                        serverReader.awaitContent()
+                        size = serverReader.readAvailable(buffer)
+
+                        clientWriter.writeAvailable(buffer, 0, size)
+                        clientWriter.flush()
+
+                        clientSocket.close()
+                        serverSocket.close()
+                    }
+                } catch (e: Exception) {
+                }
+            }
+        }
+    }
+}
